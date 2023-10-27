102841c1aede09e56a52f766ebf0faceeb484f9d
mmakarov <mmakarov@student.42lausanne.ch>
2023-10-27 18:42:40 +0200

    27.10

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -10,7 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
 int main()
 {
+    std::string s = "HI THIS IS BRAIN";
+    std::string *stringPTR = &s;
+    std::string &stringREF = s;
+
+    std::cout << "The memory address of the string variable: " << &s << std::endl;
+    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "The value of the string variable: " << s << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+
     return (0);
 }
