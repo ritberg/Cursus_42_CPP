0d85a3fae97c88877e16b6b9cfd1a82db659d05c
mmakarov <mmakarov@student.42lausanne.ch>
2023-10-29 19:27:52 +0100

    29.10

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -14,7 +14,12 @@
 #include <iostream>
 #include <fstream>
 
-//to do: understand references in regards to this code (cf. chat gpt); change file name
+//to do: understand references in regards to this code (cf. chat gpt); change file name; args check
+
+bool isString(const std::string& variable)
+{
+    return (true);
+}
 
 int main(int argc, char **argv)
 {
@@ -23,11 +28,16 @@ int main(int argc, char **argv)
         std::ifstream   ifs;
         std::string     s1 = argv[2];
         std::string     s2 = argv[3];
-        std::ofstream   ofs("file.replace");
-    
+        std::string     line;
+        
         ifs.open (argv[1], std::ifstream::in);
-    
-        std::string line;
+        if (!ifs.is_open())
+        {
+            std::cerr << "Error: no such file." << std::endl;
+            return (1);
+        }
+        std::string     ofsFileName = std::string(argv[1]) + ".replace";
+        std::ofstream   ofs(ofsFileName);
     
         while (std::getline(ifs, line))
         {
