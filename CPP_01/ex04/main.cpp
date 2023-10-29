dbb3d4ddc94fe15d4e0d32ed9076df36fcbcf833
mmakarov <mmakarov@student.42lausanne.ch>
2023-10-29 12:08:36 +0100

    29.10

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -10,10 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include
+#include <string>
+#include <iostream>
+#include <fstream>
 
-int main()
+//to do: understand references in regards to this code (cf. chat gpt); change file name
+
+int main(int argc, char **argv)
 {
+    if (argc == 4)
+    {
+        std::ifstream   ifs;
+        std::string     s1 = argv[2];
+        std::string     s2 = argv[3];
+        std::ofstream   ofs("file.replace");
+    
+        ifs.open (argv[1], std::ifstream::in);
+    
+        std::string line;
+    
+        while (std::getline(ifs, line))
+        {
+            size_t  found = line.find(s1);
+            while (found != std::string::npos) /* npos = no matches */
+            {
+                std::string before = line.substr(0, found);
+                std::string after = line.substr(found + s1.size());
+                line = before + s2 + after;
+                found = line.find(s1, found + s2.size());
+            }
+            ofs << line << std::endl;
+        }
+        ifs.close();
+        ofs.close();
+    }
+    else
+        std::cout << "Error: you should enter a filename and two strings, s1 and s2" << std::endl;
+   
     return (0);
 }
-
