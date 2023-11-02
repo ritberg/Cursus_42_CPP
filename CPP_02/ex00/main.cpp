101256191eb4eb62b95e8662711784928339d036
mmakarov <mmakarov@student.42lausanne.ch>
2023-11-02 18:49:45 +0100

    02.11 start cpp02

---
+++ CPP_02/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakarov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 17:56:11 by mmakarov          #+#    #+#             */
+/*   Updated: 2023/11/02 17:57:15 by mmakarov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include "Fixed.hpp"
+
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
