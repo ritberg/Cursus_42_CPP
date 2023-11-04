9f7c7a508872f9c566440448846411f49c111784
ritberg <67046171+ritberg@users.noreply.github.com>
2023-11-04 19:33:07 +0100

    04.11

--- CPP_02/ex01/Fixed.cpp
+++ CPP_02/ex01/Fixed.cpp
@@ -14,10 +14,10 @@
 
 const int Fixed::_fract = 8;
 
-/* Default Constructor: Initializes the _value to 0 */
+/* Default Constructor: Initializes the `_value` to 0. */
 Fixed::Fixed(void) : _value(0)
 {
-    std::cout << "*** Default Constr *** _value is :" << _value << std::endl; //
+    std::cout << "*** Default Constr *** _value is : " << _value << std::endl; //
 
     std::cout << "Default constructor called" << std::endl;
     return;
@@ -25,12 +25,15 @@ Fixed::Fixed(void) : _value(0)
 
 /*
 Integer Constructor.
-Takes an integer n and initializes the _value by left-shifting n by _fract bits
+Takes an integer `n` and does left-shifting by `_fract` bits (by 8).
+Stores `n` in _value.
+
+10 would be 2560 because 10 << 8 = 2560 (cf. Fixed const b(10) in main.cpp).
 */
 Fixed::Fixed(const int n) : _value(n << _fract)
 {
-    std::cout << "*** Int Constr *** n is :" << n << std::endl; //
-    std::cout << "*** Int Constr *** _value is :" << _value << std::endl; //
+    std::cout << "*** Int Constr *** n is : " << n << std::endl; //
+    std::cout << "*** Int Constr *** _value is : " << _value << std::endl; //
 
     std::cout << "Int constructor called" << std::endl;
     return;
@@ -38,13 +41,16 @@ Fixed::Fixed(const int n) : _value(n << _fract)
 
 /*
 Float Constructor.
-Takes a floating-point number nf, rounds it, and scales it by (1 << _fract).
-Stores it in _value
+Takes a floating-point number `nf`, rounds it, and scales it by `1 << _fract` or 1 << 8.
+Stores the result in _value.
+
+1 << 8 = 256 in decimal or 100000000 in binary.
+42.42 would be 10860 because round(42.42 * 256) = 10860 (cf. Fixed const c(42.42f) in main.cpp).
 */
 Fixed::Fixed(const float nf) : _value(round(nf * (1 << _fract)))
 {
-    std::cout << "*** Float constr *** nf is :" << nf << std::endl; //
-    std::cout << "*** Float constr *** _value is :" << _value << std::endl; //
+    std::cout << "*** Float constr *** nf is : " << nf << std::endl; //
+    std::cout << "*** Float constr *** _value is : " << _value << std::endl; //
 
     std::cout << "Float constructor called" << std::endl;
     return;
@@ -84,16 +90,31 @@ void    Fixed::setRawBits(int const raw)
     return;
 }
 
+/*  
+1 << 8 = 256 in decimal or 100000000 in binary.
+
+Example of 42.42.
+If it is stored as a fixed-point number by multiplying it by 256
+(_fract is 8), we get 10860 (rounded).
+To convert 10860 back to 42.42, we need to divide it by 256
+to reverse the scaling operation.
+*/
 float   Fixed::toFloat(void) const
 {
-    std::cout << "*** toFloat *** _value is :" << _value << std::endl; //
+    std::cout << "*** toFloat *** _value is : " << _value << std::endl; //
 
     return ((float)this->_value / (1 << _fract)); 
 }
 
+/*
+Shifting to the right by _fract positions removes the fractional bits
+and leaves only the integer part of the fixed-point value. 
+
+Example: 10860 >> 8 = 42 (cf. Fixed const c(42.42f) in main.cpp).
+*/
 int     Fixed::toInt(void) const
 {
-    std::cout << "*** toInt *** _value is :" << _value << std::endl; //
+    std::cout << "*** toInt *** _value is : " << _value << std::endl; //
 
     return (this->_value >> Fixed::_fract);
 }
