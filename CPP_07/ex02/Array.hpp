490fbaad2246c8fc1ac75ee11b39fbbbf4b6b726
mmakarov <mmakarov@student.42lausanne.ch>
2023-12-10 18:40:19 +0100

    10.12

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -15,12 +15,80 @@
 
 # include <iostream>
 # include <string>
+# include <iomanip>
 
-template < typename T = int >
+# define RESET	"\e[0m"
+# define BLUE   "\033[96m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define VIOLET	"\033[35m"
+# define RED "\033[91m"
+# define GREY "\033[30m"
+
+template < typename T = unsigned int >
 class Array
 {
-    private:
     public:
+        Array(void) : _data(NULL), _n(0) {}
+        Array(unsigned int n) : _data(new T[n]), _n(n) {}
+        Array(Array const & src) : _data(new T[src.size()]), _n(src._n)
+        {   
+            for (unsigned int i = 0; i < this->size(); ++i)
+                this->_data[i] = src._data[i];
+        }
+        ~Array(void) { delete [] _data; }
+        Array&  operator=(Array const & rhs)      ///// deep copy
+        {
+            if (this != &rhs)
+            {
+                delete[] _data;
+                this->_n = rhs._n;
+                this->_data = new T[this->size()];
+                for (unsigned int i = 0; i < this->size(); ++i)
+                    this->_data[i] = rhs._data[i];
+            }
+            return (*this);
+        }
+
+        unsigned int size(void) const { return (this->_n); }
+
+        T& operator[](unsigned int index)
+        {
+            if (index >= this->size())
+                throw std::out_of_range("Index out of bounds");
+            return (this->_data[index]);
+        }
+
+    private:
+        unsigned int    _n;
+        T*              _data;
 };
+
+template <typename T>
+std::ostream &	operator<<(std::ostream &out, Array<T> & obj)
+{
+    out << "Size of the array is " << obj.size() << ", elements of the array are ";
+    if (obj.size() == 0)
+        out << "0, because it's empty";
+    for (unsigned int i = 0; i < obj.size(); ++i)
+    {
+        if (typeid(obj[i]) != typeid(std::string))
+            obj[i] = i + 1;
+        out << obj[i] << " ";
+    }
+    return (out);
+}
+
+/*   Surcharge sur l'opérateur << pour les double values  */
+template <>
+std::ostream &	operator<<(std::ostream &out, Array<double>& obj)
+{
+    out << "Size of the array is " << obj.size() << ", elements of the array are ";
+    if (obj.size() == 0)
+        out << "0, because it's empty";
+    for (unsigned int i = 0; i < obj.size(); ++i)
+        out << std::setprecision(6) << obj[i] << " ";
+    return (out);
+}
 
 #endif
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -9,3 +9,94 @@
 /*   Updated: 2023/12/08 13:47:15 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main()
+{
+    std::cout << std::endl << BLUE "----- Array<> arr(10) " RESET << std::endl;
+
+    Array<> arr(10);
+    std::cout << BLUE << arr << RESET << std::endl;
+
+    std::cout << std::endl << BLUE "----- Array<> arrNoParams1 " RESET << std::endl;
+
+    Array<> arrNoParams1;
+    std::cout << BLUE << arrNoParams1 << RESET << std::endl;
+
+    std::cout << std::endl << YELLOW "----- Array<float> arrNoParams2 " RESET << std::endl;
+
+    Array<float> arrNoParams2;
+    std::cout << YELLOW << arrNoParams2 << RESET << std::endl;
+
+    std::cout << std::endl << YELLOW "----- Array<double> arrDbl(5) " RESET << std::endl;
+
+    Array<double> arrDbl(5);
+    arrDbl[0] = 1.234567;
+    arrDbl[1] = 2.345678;
+    arrDbl[2] = 3.456789;
+    arrDbl[3] = 4.567890;
+    arrDbl[4] = 5.678901;
+    std::cout << YELLOW << arrDbl << RESET << std::endl;
+
+    std::cout << std::endl << GREEN "----- Array<int> arrInt(20) " RESET << std::endl;
+
+    Array<int> arrInt(20);
+    std::cout << GREEN << arrInt << RESET << std::endl;
+
+    std::cout << std::endl << GREEN "----- Array<std::string> arrStr(2), arrStr[0] = \"42\", arrStr[1] = \"Lausanne\" " RESET << std::endl;
+
+    Array<std::string> arrStr(2);
+    arrStr[0] = "42";
+    arrStr[1] = "Lausanne";
+    std::cout << GREEN << arrStr << RESET << std::endl;
+
+    std::cout << std::endl << RED "----- Out of array - arrStr[2] = \"à Renens\" " RESET << std::endl;
+    try
+    {
+        arrStr[2] = "à Renens";
+        std::cout << RED << arrStr << RESET << std::endl;
+    }
+    catch (const std::out_of_range& e)
+    {
+        std::cerr << RED "❗️Error: " << e.what() << RESET << std::endl;
+    }
+
+    std::cout << std::endl << RED "----- Out of array - Array<int> arrInteger(3), arrInteger[4] " RESET << std::endl;
+    Array<int> arrInteger(3);
+
+    arrInteger[0] = 10;
+    arrInteger[1] = 20;
+    arrInteger[2] = 30;
+    try
+    {
+        std::cout << RED << arrInteger[4] << RESET << std::endl;
+    }
+    catch (const std::out_of_range& e)
+    {
+        std::cerr << RED "❗️Error: " << e.what() << RESET << std::endl;
+    }    
+
+
+    std::cout << std::endl << "   *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *" << std::endl;
+    std::cout << "  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *" << std::endl;
+    std::cout << "   *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *" << std::endl;
+
+
+    std::cout << std::endl << VIOLET "----- int* arrayInt = new int[20] " RESET << std::endl;
+
+    int* arrayInt = new int[20];
+    std::cout << VIOLET << arrInt << RESET << std::endl;
+    delete [] arrayInt;
+
+    std::cout << std::endl << GREY "----- int* a = new int() " RESET << std::endl;
+
+    int* a = new int();
+    std::cout << GREY << *a << RESET << std::endl;
+    delete a;
+
+
+    std::cout << std::endl;
+
+    return (0);
+}
